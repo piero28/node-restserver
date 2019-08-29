02694932e1ad9dca063b0fc0d1f208f7fd47794d
Pierre <pierreangeljarafreire@gmail.com>
2019-08-29 12:48:46 -0400

    Rest server - Usuario

---
+++ server/routes/usuario.js
@@ -0,0 +1,102 @@
+const express = require('express');
+const bcrypt = require('bcrypt');
+const _underscore = require('underscore');
+const Usuario = require('../models/usuario');
+const app = express();
+
+  app.get('/usuario', function (req, res) {
+    let desde = req.query.desde || 0;
+    desde = Number(desde);
+    let limite = req.query.limite || 5;
+    limite = Number(limite);
+    Usuario.find({estado:true}, 'nombre email role estado google img')
+        .skip(desde)
+        .limit(limite)
+        .exec((err, usuarios) => {
+            if(err){
+                return res.status(400).json({
+                    success: false,
+                    err
+                });
+            }
+
+            Usuario.count({estado:true}, (err, conteo) => {
+                res.json({
+                    success: true,
+                    usuario: usuarios,
+                    totalRegistros: conteo
+                });
+            })
+        })
+  });
+  
+  app.post('/usuario', function (req, res) {
+      let body = req.body;
+      let usuario = new Usuario({
+            nombre : body.nombre,
+            email: body.email,
+            password: bcrypt.hashSync(body.password, 10),
+            role: body.role
+      });
+      usuario.save((err, usuarioDB) => {
+            if(err){
+                return res.status(400).json({
+                    success: false,
+                    err
+                });
+            }
+            res.json({
+                success: true,
+                usuario: usuarioDB
+            });
+      });
+  });
+  
+  app.put('/usuario/:id', function (req, res) {
+      let id = req.params.id;
+      let body = _underscore.pick(req.body, ['nombre','email','img','role','estado']);
+      Usuario.findByIdAndUpdate(id, body, {new: true, runValidators: true, context: 'query'}, (err, usuarioDB) => {
+            if(err){
+                return res.status(400).json({
+                    success: false,
+                    err
+                });
+            }
+            res.json({
+                success: true,
+                usuario: usuarioDB
+            });
+      })
+  });
+  
+  app.delete('/usuario/:id', function (req, res) {
+      let id = req.params.id;
+      let cambiaEstado = {
+          estado: false
+      }
+      //findByIdAndRemove eliminado fisico
+      //findByIdAndDelete eliminado logico
+      Usuario.findByIdAndUpdate(id, cambiaEstado, {new: true}, (err, usuarioBorrado) => {
+        if(err){
+            return res.status(400).json({
+                success: false,
+                err
+            });
+        };
+        if(!usuarioBorrado){
+            return res.status(400).json({
+                success: false,
+                err: {
+                    message: 'Usuario no encontrado'
+                }
+            });
+        }
+        res.json({
+            success: true,
+            usuario: usuarioBorrado
+        });
+      })
+  });
+
+
+  module.exports = app;
--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 require('./config/config');
 
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
 
 var bodyParser = require('body-parser');
@@ -8,36 +10,15 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
+//de esta manera importamos y usamos esas rutas del usuario
+app.use(require('./routes/usuario'));
  
-app.get('/usuario', function (req, res) {
-  res.json('get Usuario');
-});
-
-app.post('/usuario', function (req, res) {
-    let body = req.body;
-    if(body.nombre === undefined){
-        res.status(400).json({
-            status: false,
-            mensaje: 'El nombre es necesario'
-        });
-    }else{
-        res.json({
-            persona: body
-        });
+mongoose.connect('mongodb://localhost:27017/cafe', {useNewUrlParser: true}, (err, res) => {
+    if(err){
+        throw err;
     }
+    console.log("Base de datos ONLINE!!   mongodb+srv://pierrejara:<password>@cluster0-s5xtq.mongodb.net/test");
 });
-
-app.put('/usuario/:id', function (req, res) {
-    let id = req.params.id;
-    res.json({
-        id
-    });
-});
-
-app.delete('/usuario', function (req, res) {
-    res.json('get Usuario');
-});
-   
  
 app.listen(process.env.PORT, () => {
     console.log("Escuchando puerto: ", 3000);
